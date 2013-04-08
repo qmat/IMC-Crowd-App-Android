8e33ca9f508f9a15419ec00d39f84328e5fcc8fd
Toby Harris <operator@tobyz.net>
2013-04-08 18:13:00 +0100

    WakeLock
    Keeps app/phone running.

--- src/uk/ac/qmul/eecs/imccrowdapp/MainActivity.java
+++ src/uk/ac/qmul/eecs/imccrowdapp/MainActivity.java
@@ -1,15 +1,30 @@
 package uk.ac.qmul.eecs.imccrowdapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private final static String TAG = "IMCCrowdApp";
+	
+	private WakeLock wakeLock;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// TASK: Keep App running
+		
+		// PARTIAL_WAKE_LOCK will allow screen and keyboard to dim, but app/phone will still run
+		// If you hold a partial wake lock, the CPU will continue to run, regardless of any display timeouts or the state of the screen and even after the user presses the power button. 
+		PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+		wakeLock.acquire();
 	}
 
 	@Override
@@ -18,5 +33,9 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	@Override
+	public void onStop() {
+		wakeLock.release();
+	}
 }
