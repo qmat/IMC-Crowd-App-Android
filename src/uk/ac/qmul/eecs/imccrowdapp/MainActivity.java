ac4f70f2a389f7836462a2387caebd587897b305
Toby Harris <operator@tobyz.net>
2013-04-08 20:02:23 +0100

    DataLoggingManager
    Something is crashing somewhere. Debug is not being forthcoming.

--- src/uk/ac/qmul/eecs/imccrowdapp/MainActivity.java
+++ src/uk/ac/qmul/eecs/imccrowdapp/MainActivity.java
@@ -1,10 +1,15 @@
 package uk.ac.qmul.eecs.imccrowdapp;
 
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -12,6 +17,7 @@ public class MainActivity extends Activity {
 	private final static String TAG = "IMCCrowdApp";
 	
 	private WakeLock wakeLock;
+	private DataLoggingManager dataLoggingManager;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,10 @@ public class MainActivity extends Activity {
 		PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
 		wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
 		wakeLock.acquire();
+		
+		// TASK: Start logging data
+		dataLoggingManager = new DataLoggingManager(this.getApplicationContext());
+		dataLoggingManager.start(100);
 	}
 
 	@Override
@@ -36,6 +46,73 @@ public class MainActivity extends Activity {
 	
 	@Override
 	public void onStop() {
+		dataLoggingManager.stop();
 		wakeLock.release();
+	}
+}
+
+
+class DataLoggingManager
+{
+	Context context;
+	Timer timer;
+	
+	DataLoggingManager(Context inContext)
+	{
+		context = inContext;
+	}
+	
+	class DataLogTask extends TimerTask
+	{
+		private static final String TAG = "DataLogTask";
+		
+		DataLogger dataLogger;
+		ServerConnection serverConnection;
+		
+		DataLogTask()
+		{
+			dataLogger = new DataLogger();
+			
+			File filesDir = context.getExternalFilesDir("sensorData");
+			if (filesDir == null)
+			{
+				Log.w(TAG, "Could not use external files dir, falling back to internal");
+				filesDir = context.getFilesDir();
+			}
+			if (filesDir == null)
+			{
+				Log.e(TAG, "Could not use files dir.");
+				// TODO: Alert dialog and quit?
+			}
+			dataLogger.setLogsFolder(filesDir.getPath());
+			
+			serverConnection = new ServerConnection();
+//			serverConnection.setEndPointURL(""); // TODO: Pull string from strings file kept locally, not in public source control
+//			serverConnection.startSession();
+//			serverConnection.startFileUploads();
+		}
+		
+		public void run() 
+		{
+			Log.v("DataLogTask", "Run called");
+		}
+	}
+	
+	public void start(int periodMicroSeconds)
+	{
+		if (timer != null) 
+		{
+			timer.cancel();
+			timer = null;
+		}
+		
+		timer = new Timer();
+		timer.schedule(new DataLogTask(), 0, periodMicroSeconds);
+	}
+	
+	public void stop()
+	{
+		timer.cancel();
+		timer = null;
 	}
 }
