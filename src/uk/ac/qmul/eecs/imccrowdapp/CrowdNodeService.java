7b34d2d7515c46f2811afe63e0b0cb6c415fc0ed
Toby Harris <operator@tobyz.net>
2013-04-14 13:55:21 +0100

    Service and it's GUI implemented
    Service is foregrounded
    Service has ongoing notification
    Toggle button in main GUI to manage service
    Handles MainActivity being constantly recreated

--- src/uk/ac/qmul/eecs/imccrowdapp/CrowdNodeService.java
+++ src/uk/ac/qmul/eecs/imccrowdapp/CrowdNodeService.java
@@ -2,17 +2,30 @@ package uk.ac.qmul.eecs.imccrowdapp;
 
 import java.util.Timer;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.util.Log;
 
 public class CrowdNodeService extends Service {
 	private static final String TAG = "CrowdNodeService";
+	private static final int idForForeground = 1;
+	
+	// Determine if service is running, available within package
+	private static CrowdNodeService instance = null;
+	static boolean isInstanceCreated() { return instance != null; }
+	static boolean hasInstanceEverBeenCreated = false;
 	
 	// TODO: Make dynamic
 	private static final int captureDataPeriodMicroSeconds = 1000;
-		
+	
+	private WakeLock wakeLock;
+	
 	Timer captureDataTimer;
 	
 	ServerConnection serverConnection;
@@ -26,6 +39,17 @@ public class CrowdNodeService extends Service {
 	public void onCreate() {
 		Log.d(TAG, "onCreate");
 
+		// TASK: Serve our WTF ANDROID is-service-running query
+		hasInstanceEverBeenCreated = true;
+		instance = this;
+		
+		// TASK: Don't let phone sleep so that the service can keep actively running
+		
+		// If you hold a partial wake lock, the CPU will continue to run, regardless of any display timeouts or the state of the screen and even after the user presses the power button. 
+		PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+		wakeLock.acquire();
+		
 		// TASK: Start logging data
 		
 		// We do this in onCreate rather than onStart, as this is the one thing this service does, and any more calls to start it are redundant. Akin to singleton.	
@@ -42,11 +66,28 @@ public class CrowdNodeService extends Service {
 		
 		captureDataTimer = new Timer();
 		captureDataTimer.schedule(new DataLoggerTask(this), 0, captureDataPeriodMicroSeconds);
+		
+		// TASK: Run as foreground service
+		
+		// ie. service won't be culled by Android if low on resources, running it is an overt user intent
+		startForeground(idForForeground, createServiceNotification());
 	}
 
 	@Override
 	public void onDestroy() {
 		Log.d(TAG, "onDestroy");
+		
+		// TASK: Serve our WTF ANDROID is service running query
+		instance = null;
+		
+		// TASK: Revert Android things
+		
+		// Allow phone to sleep again
+		wakeLock.release();
+		wakeLock = null;
+		
+		// Stop service's foreground'ness and remove notification
+		stopForeground(true);
 		
 		// TASK: Stop logging and uploading data
 		
@@ -62,4 +103,24 @@ public class CrowdNodeService extends Service {
 		Log.d(TAG, "onStart");
 		
 	}
+	
+    private Notification createServiceNotification() {
+    	Notification.Builder notificationBuilder = new Notification.Builder(this)
+											    	       .setSmallIcon(R.drawable.ic_launcher)
+											    	       .setContentTitle("IMC Crowd App")
+											    	       .setContentText("Sensing active")
+    	 												   .setProgress(0, 0, true);
+
+    	// TASK: Set action for notification
+    	Intent resultIntent = new Intent(this, MainActivity.class);
+    	
+    	// Don't relaunch the activity if its currently the foreground app
+    	resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+    	
+    	PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, 0);
+    	
+    	notificationBuilder.setContentIntent(resultPendingIntent);
+
+        return notificationBuilder.build();
+    }
 }
--- src/uk/ac/qmul/eecs/imccrowdapp/MainActivity.java
+++ src/uk/ac/qmul/eecs/imccrowdapp/MainActivity.java
@@ -1,12 +1,11 @@
 package uk.ac.qmul.eecs.imccrowdapp;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PowerManager;
-import android.os.PowerManager.WakeLock;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
@@ -17,19 +16,21 @@ public class MainActivity extends Activity {
 	// "/Path To Android that might have spaces in it so we're in quotes here/adt-bundle-mac-x86_64-20130219/sdk/platform-tools/adb" shell setprop log.tag.IMCCrowdApp VERBOSE
 	// To release, these Log calls will need to be qualified with "if (BuildConfig.DEBUG) Log..."
 	
-	private WakeLock wakeLock;
-		
+	ToggleButton crowdNodeServiceToggleButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		// INFO: Stay alive strategy - Use PARTIAL_WAKE_LOCK and have our node of the crowd running as a service. 
-		// The power switch needn't affect us, and we can choose whether to start/stop the service with the GUI, or have it run fully in the background with start/stop control via any instance of the app.
+		crowdNodeServiceToggleButton = (ToggleButton) findViewById(R.id.crowdNodeServiceToggleButton);
 		
-		// If you hold a partial wake lock, the CPU will continue to run, regardless of any display timeouts or the state of the screen and even after the user presses the power button. 
-		PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
-		wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+		// TASK: Start our node of the (assumed) crowd (the user is among)
+		// Only doing this on first launch, afterwards toggle should set state.
+		if (!CrowdNodeService.hasInstanceEverBeenCreated) 
+		{
+			startService(new Intent(this, CrowdNodeService.class));
+		}
 	}
 
 	@Override
@@ -43,22 +44,19 @@ public class MainActivity extends Activity {
 	public void onStart() {
 		super.onStart();
 		
-		// FIXME: As the activity is restarted on orientation change and stopped on screen dim, we need to start the service on first launch, if its not running already, and have a button to stop it. AFAIK there's no way to see the reason behind the onStop being called, to stop the service only on direct user intervention.
-		
-		// TASK: Don't let the phone sleep
-		wakeLock.acquire();
-		
-		// TASK: Start our node of the (assumed) crowd (the user is among)
-		startService(new Intent(this, CrowdNodeService.class));
+		// TASK: Set service button state
+		// FIXME: This is often incorrect on first launch as service is still starting. Need a broadcast to tell the app?
+		crowdNodeServiceToggleButton.setChecked(CrowdNodeService.isInstanceCreated());
 	}
 	
-	@Override
-	public void onStop() {
-		
-		stopService(new Intent(this, CrowdNodeService.class));
-		
-		wakeLock.release();
-		
-		super.onStop();
+	public void onCrowdNodeServiceToggleClicked(View view) {
+	    // Is the toggle on?
+	    boolean on = ((ToggleButton) view).isChecked();
+	    
+	    if (on) {
+	    	startService(new Intent(this, CrowdNodeService.class));
+	    } else {
+	    	stopService(new Intent(this, CrowdNodeService.class));
+	    }
 	}
 }
